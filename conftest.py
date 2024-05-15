7425694af43c97e5d2dd20b23026934f5e67bdad
Denis <denis.labunin@mail.ru>
2024-05-15 16:24:56 +0300

    create github actions

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='class')
 def browser():
-    browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(8)
     yield browser
     browser.quit()
